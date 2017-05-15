a492fb1a109d1c60935a66aec9bf6962134adf5c
unknown <mjm416@phws301-038.ic.ac.uk>
2017-05-15 14:57:02 +0100

    Second derivative matrix added
    1. Problems with comlpex numbers in matrices.
    2. Second derivative matrix can now be produced.

--- base.cpp
+++ base.cpp
@@ -2,12 +2,12 @@
 #include <cmath>
 #include "Eigen/Dense"
 
-double func(double x) {
-	return std::sinh(x);
+double sinh(double x) {
+	return std::sin(x);
 }
 
-Eigen::VectorXd wavefunction(double (*func)(double x), double N_steps, double system_size = 1) {
-	/*	Initialises the wavefunction (system vector) from function callback "initial"
+Eigen::VectorXd linspace(double (*func)(double x), double N_steps, double system_size = 1) {
+	/*	Initialises the wavefunction (system vector) from function callback "func"
 	*	func		- function f(x) which defines the wavefunction at t=0;
 	*	N_steps		- number of total steps
 	*	system_size	- size of the whole system (in case that pointer arithmetic fails miserably)
@@ -18,17 +18,36 @@ Eigen::VectorXd wavefunction(double (*func)(double x), double N_steps, double sy
 	return ans;
 }
 
+Eigen::MatrixXd second_derv(int size) {
+	/*	Produces a three-point difference algorithm matrix (tridiagonal with 1, -2, 1 values.
+	*	size		- size of the matrix (size x size)
+	*/
+	Eigen::MatrixXd ans(size, size);
+	ans = Eigen::MatrixXd::Zero(size, size);
+	for(int row = 0; row<size; row++) {
+		if(row != 0) ans(row,row-1) = 1;
+		if(row != size-1) ans(row,row+1) = 1;
+		ans(row,row) = -2;
+	}
+	return ans;
+}
+
 int main()
 {
 	Eigen::MatrixXd A(3,3);
 	Eigen::VectorXd B(3);
 	A << 1,2,3,4,6,54,3,7,3;
 	B << 1,2,3;
+	std::cout << A*B << std::endl;
 	
-	Eigen::VectorXd psi = wavefunction(func, 10);
+	Eigen::VectorXd psi = linspace(sin, 10);
+	std::cout << psi << std::endl;
 	
-	std::cout << A*B;
-	std::cout << psi;
+	Eigen::MatrixXd TPD = second_derv(10);
+	std::cout << TPD << std::endl;
+	
+	Eigen::MatrixXd multip = TPD*psi;
+	std::cout << multip << std::endl;
 	char c;
 	std::cin >> c;
 	return 0;
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 CC=g++
 CFLAGS=-g -static-libstdc++
 DEPS = 
-OBJ = main.o 
+OBJ = base.o 
 
 %.o: %.cpp $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-main: $(OBJ)
+base: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
